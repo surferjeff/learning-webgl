4da160af3711ee6b1dd251a50e789d090033a0ee
Jeffrey Rennie <jeff@rennieglen.com>
2024-03-06 12:42:42 -0800

    Shaders compile

---
+++ 02-triangle/index.js
@@ -0,0 +1,40 @@
+let canvas = document.getElementById('canvas');
+let context = canvas.getContext('webgl2');
+context.clearColor(0.1, 0.2, 0.3, 1.0);
+context.clear(context.COLOR_BUFFER_BIT|context.DEPTH_BUFFER_BIT);
+
+let triangleCoords = [0.0, -1.0, 0.0, 1.0, 1.0, -1.0];
+
+// Step 1: Write shaders
+let vertexShader = `#version 300 es
+precision mediump float;
+in vec2 position;
+void main() {
+  gl_Position = vec4(position, 0.0, 1.0);
+}
+`;
+let fragmentShader = `#version 300 es
+precision mediump float;
+out vec4 color;
+void main() {
+  color = vec4(0.0, 1.0, 0.0, 1.0);
+}
+`;
+
+// Step 2: Create program from shaders
+let vs = context.createShader(context.VERTEX_SHADER);
+context.shaderSource(vs, vertexShader);
+context.compileShader(vs);
+if (!context.getShaderParameter(vs, context.COMPILE_STATUS)) {
+    throw new Error(context.getShaderInfoLog(vs));
+}
+let fs = context.createShader(context.FRAGMENT_SHADER);
+context.shaderSource(fs, fragmentShader);
+context.compileShader(fs);
+if (!context.getShaderParameter(fs, context.COMPILE_STATUS)) {
+    throw new Error(context.getShaderInfoLog(fs));
+}
+
+// Step 3: Create buffers
+// Step 4: Link GPU variable to CPU and sending data 
+// Step 5: Render triangle
