1d9c3185fcfbfe15681a8c2d7a9ef5ddf370101e
Jeffrey Rennie <jeff@rennieglen.com>
2024-03-06 21:42:23 -0800

    Do more math in the shader.
    May not actually be faster because the same values are calculated many times.

--- 07-free-rect/index.js
+++ 07-free-rect/index.js
@@ -11,10 +11,14 @@ gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
 // Step 1: Write shaders
 let vertexShader = `#version 300 es
 precision mediump float;
-uniform float flipY;
+uniform float canvasWidth;
+uniform float canvasHeight;
 in vec2 position;
 void main() {
-  gl_Position = vec4(position.x, position.y * flipY, 0.0, 1.0);
+  gl_Position = vec4(
+    position.x / canvasWidth * 2.0 - 1.0,
+    position.y / canvasHeight * -2.0  + 1.0,
+    0.0, 1.0);
 }
 `;
 let fragmentShader = `#version 300 es
@@ -36,10 +40,6 @@ let program = util.getProgram(gl, vertexShader, fragmentShader);
  * @param {number} endY 
  */
 function updateRect(startX, startY, endX, endY) {
-  startX = (startX / canvas.width) * 2 - 1;
-  startY = (startY / canvas.height) * 2 - 1;
-  endX = (endX / canvas.width) * 2 - 1;
-  endY = (endY / canvas.height) * 2 - 1;
   let triangles = [
     startX, startY,
     endX, startY,
@@ -50,8 +50,10 @@ function updateRect(startX, startY, endX, endY) {
   ];
   gl.clear(gl.COLOR_BUFFER_BIT|gl.DEPTH_BUFFER_BIT);
   gl.useProgram(program);
-  let location = gl.getUniformLocation(program, 'flipY');
-  gl.uniform1f(location, -1);
+  let canvasWidth = gl.getUniformLocation(program, 'canvasWidth');
+  gl.uniform1f(canvasWidth, canvas.width);
+  let canvasHeight = gl.getUniformLocation(program, 'canvasHeight');
+  gl.uniform1f(canvasHeight, canvas.height);
   let buffer = util.createAndBindBuffer(gl, gl.ARRAY_BUFFER,
       new Float32Array(triangles), gl.STATIC_DRAW);
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
