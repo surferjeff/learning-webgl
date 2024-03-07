5efe7b7d02addd223ca7a4ff7c183ff2510e723b
Jeffrey Rennie <jeff@rennieglen.com>
2024-03-06 17:41:47 -0800

    Move the rectangle by changing the vertex shader.

--- 03-rectangle/index.js
+++ 03-rectangle/index.js
@@ -12,7 +12,7 @@ let vertexShader = `#version 300 es
 precision mediump float;
 in vec2 position;
 void main() {
-  gl_Position = vec4(position, 0.0, 1.0);
+  gl_Position = vec4(position.x + 0.1, position.y - 0.2, 0.0, 1.0);
 }
 `;
 let fragmentShader = `#version 300 es
