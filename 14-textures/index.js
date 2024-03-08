805f711e39f8d51c002f5bd2c1933bc66cf3b3f5
Jeffrey Rennie <jeff@rennieglen.com>
2024-03-07 16:38:18 -0800

    Upright.

--- 14-textures/index.js
+++ 14-textures/index.js
@@ -15,7 +15,7 @@ in vec2 position;
 in vec2 texCoords;
 out vec2 textureCoords;
 void main() {
-  gl_Position = vec4(position, 0.0, 1.0);
+  gl_Position = vec4(position.x, -position.y, 0.0, 1.0);
   textureCoords = position; // texCoords;
 }
 `;
