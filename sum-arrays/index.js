811267fb2bd9e2e27f02d0548a0dced71b1630e0
Jeffrey Rennie <jeff@rennieglen.com>
2024-03-07 19:22:16 -0800

    Red and green lines from texture.

--- sum-arrays/index.js
+++ sum-arrays/index.js
@@ -20,9 +20,9 @@ void main() {
 let fragmentShader = `#version 300 es
 precision mediump float;
 out vec4 color;
-uniform 
+uniform sampler2D tex;
 void main() {
-  color = vec4(0.0, gl_FragCoord.x / 100.0, 0.0, 1.0);
+  color = texture(tex, vec2(gl_FragCoord.x / 100.0, gl_FragCoord.y / 8.0));
 }
 `;
 
@@ -37,17 +37,35 @@ for (let i = 0; i < 100; i++) {
 let buffer = util.createAndBindBuffer(gl, gl.ARRAY_BUFFER, indices,
   gl.STATIC_DRAW);
 
-let a1 = new Float32Array(100);
-for (let i = 0; i < 100; i++) {
-  a1[i] = 100;
+// Create a texture: 2 lines of 100 pixels each
+let texture = gl.createTexture();
+gl.bindTexture(gl.TEXTURE_2D, texture);
+let data = new Uint8Array(100 * 2 * 4);
+// red line.
+for (let i = 0; i < 400; i += 4) {
+  data[i] = 255;
+  data[i + 1] = 0;
+  data[i + 2] = 0;
+  data[i + 3] = 255;
 }
-let a1buffer = util.createAndBindBuffer(gl, gl.ARRAY_BUFFER, a1,
-  gl.STATIC_DRAW);
+// green line.
+for (let i = 400; i < 800; i += 4) {
+  data[i] = 0;
+  data[i + 1] = 255;
+  data[i + 2] = 0;
+  data[i + 3] = 255;
+}
+
+gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 100, 2, 0, gl.RGBA, gl.UNSIGNED_BYTE, data);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
+
 
 // Step 4: Link GPU variable to CPU and sending data
-gl.bindBuffer(gl.ARRAY_BUFFER, buffer)
 gl.useProgram(program)
-const index = util.linkVariable(gl, program, 'index');
+const index = util.linkBuffer(gl, program, 'index', buffer);
 gl.vertexAttribPointer(index, 1, gl.FLOAT, gl.FALSE, 0, 0);
 
 // Step 5: Render points
